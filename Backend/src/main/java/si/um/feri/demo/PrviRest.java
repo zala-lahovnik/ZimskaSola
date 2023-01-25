d66fe575e65dc562f7fb9e504fd6700f907f19c7
zala-lahovnik <zala.lahovnik@gmail.com>
2023-01-25 10:28:32 +0100

    add cross origin

--- Backend/src/main/java/si/um/feri/demo/PrviRest.java
+++ Backend/src/main/java/si/um/feri/demo/PrviRest.java
@@ -6,16 +6,15 @@ import io.swagger.v3.oas.annotations.media.Schema;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import si.um.feri.demo.info.Deposit;
 import si.um.feri.demo.info.Transfer;
 
 import java.util.ArrayList;
 import java.util.List;
 
+//@CrossOrigin("http://localhost:3000")
+@CrossOrigin
 @RestController
 public class PrviRest {
 
